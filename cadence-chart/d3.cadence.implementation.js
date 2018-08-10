f510af907b33286731e2a2e689df3d553453c99f
Luke Stankus <lukstank@publicisgroupe.net>
2018-08-10 10:35:42 +0100

    completed fix for cadence 2-axis

--- cadence-chart/d3.cadence.implementation.js
+++ cadence-chart/d3.cadence.implementation.js
@@ -8,24 +8,28 @@
 // ==========================================================
 
 
-// Block
-let margin = {top: 20, right: 40, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
 
 
-const svg = d3.select('#cadence')
-    .style('padding', '0 10px')
-    .style('backgorund', '#ccc')
-    .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform',
-        'translate(' + margin.left + ',' + margin.top + ')');
 
 // Get the data
 let chart = d3.json('cadence-data.json').then(data => {
+
+    // Block
+    let margin = {top: 20, right: 80, bottom: 30, left: 80},
+        width = 1000 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+
+
+    const svg = d3.select('#cadence')
+        .style('padding', '0 10px')
+        .style('backgorund', '#ccc')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform',
+            'translate(' + margin.left + ',' + margin.top + ')');
 
     let time,
         numberOfStoryPointsScale,
@@ -42,26 +46,26 @@ let chart = d3.json('cadence-data.json').then(data => {
             d.date = new Date(d.date);
         });
 
-
-        // set the ranges
-
+        // 1) SETUP X AXIS AS TIME
         // TIME
         let timeScale = d3.scaleTime().range([0, width]);
-        
+        // 2) GIVE X AXIS DATA
+        timeScale.domain(d3.extent(data, function(d) { return d.date; }));
 
         // CONTRIBUTORS
-        let contributorsScale = d3.scaleBand().range([height, 0]);
-        // define the 1st line
+        let contributorsScale = d3.scaleLinear().range([height, 0]);
 
+        // ADDING VALUES CONTRIBUTORS
+        let controbutors = contributorsScale.domain([0, d3.max(data, function(d) {return Math.max(d.contributors);}) ]);
 
-        let contributorsBands = d3.band().domain(data);
+        bars = svg.selectAll('.bar').data(data.map(item => { return item })).enter();
 
-            // d3.line()
-            // .x(function(d) { return timeScale(d.date); })
-            // .y(function(d) { return contributorsScale(d.contributors); });
-
-
-
+        bars.append('rect')
+            .attr('class', 'bar-contributors')
+            .attr('x', function(d) { return timeScale(d.date) - (width / data.length) / 2; } )
+            .attr('width', (width / data.length) )
+            .attr('y', function(d) { return contributorsScale(d.contributors); })
+            .attr('height', function(d) { return height - contributorsScale(d.contributors); } );
 
         // STORY POINTS
         let storyPointsScale = d3.scaleLinear().range([height, 0]);
@@ -70,25 +74,14 @@ let chart = d3.json('cadence-data.json').then(data => {
             .x(function(d) { return timeScale(d.date); })
             .y(function(d) { return storyPointsScale(d.storyPointsCompleted); });
 
-
-        // Scale the range of the data
-        timeScale.domain(d3.extent(data, function(d) { return d.date; }));
-
-        // ADDING VALUES CONTRIBUTORS
-        contributorsScale.domain([0, d3.max(data, function(d) {return Math.max(d.contributors);})]);
-
         // ADDING VALUE STORY POINTS
         storyPointsScale.domain([0, d3.max(data, function(d) {return Math.max(d.storyPointsCompleted); })]);
 
-        // svg.append("path")
-        //     .data([data])
-        //     .attr("class", "line")
-        //     .attr("d", contributorsLine);
 
         svg.append("path")
             .data([data])
             .attr("class", "line")
-            .style("stroke", "red")
+            .style("stroke", "#f26c52")
             .attr("d", storyPointsLine);
 
         // Add the X Axis
@@ -99,13 +92,14 @@ let chart = d3.json('cadence-data.json').then(data => {
         // Add the Y0 Axis
         svg.append("g")
             .attr("class", "axisSteelBlue")
-            .call(d3.axisLeft(contributorsScale).ticks(10));
+            .attr("transform", "translate( " + -35 + ", 0 )")
+            .call(d3.axisLeft(contributorsScale).ticks(5));
 
         // Add the Y1 Axis
         svg.append("g")
             .attr("class", "axisRed")
-            .attr("transform", "translate( " + width + ", 0 )")
-            .call(d3.axisRight(storyPointsScale));
+            .attr("transform", "translate( " + (width + 35 ) + ", 0 )")
+            .call(d3.axisRight(storyPointsScale).ticks(10));
 
 
 
