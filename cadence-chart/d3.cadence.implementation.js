1c8e3c08d1b02b89a1ef6ed06374d1d0c3aa3b84
Luke Stankus <lukstank@publicisgroupe.net>
2018-08-14 17:13:31 +0100

    save before moving

--- cadence-chart/d3.cadence.implementation.js
+++ cadence-chart/d3.cadence.implementation.js
@@ -1,26 +1,36 @@
-// PLAN FOR ATTACK:
+// ADD PANNING FUNCTION
+// ADD ZOOMING FUNCTION
 
-// 2) MAKE 2 AXIS CHART
-// 2.1) MAKE 2nd axis into bars
-// 3) ADD ZOOM function 
+// SPLIT CODE TO MORE SMALLER FUNCTIONS
 
+// Block
+let margin = {top: 20, right: 80, bottom: 30, left: 80},
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
-// ==========================================================
-
-
-
+let data;
 
 
 // Get the data
-let chart = d3.json('cadence-data.json').then(data => {
+d3.json('cadence-data.json')
+    .then( json  => {
+        data = json;
+        main();
+    })
+    .catch(error => {
+        if (error) return console.warn(error);
+    })
+;
 
-    // Block
-    let margin = {top: 20, right: 80, bottom: 30, left: 80},
-        width = 1000 - margin.left - margin.right,
-        height = 600 - margin.top - margin.bottom;
+
+function main() {
+
+    let
+        bars = []
+    ;
 
 
-
+    // Main Select
     const svg = d3.select('#cadence')
         .style('padding', '0 10px')
         .style('backgorund', '#ccc')
@@ -31,52 +41,50 @@ let chart = d3.json('cadence-data.json').then(data => {
         .attr('transform',
             'translate(' + margin.left + ',' + margin.top + ')');
 
-    let time,
-        numberOfStoryPointsScale,
-        numberOfContributorsScale,
-        numberOfStoryPoints,
-        numberOfContributors,
-        storyPointLine
-    ;
-
     if (data) {
 
 
+        // Process string to date object
         data.forEach(function (d) {
             d.date = new Date(d.date);
         });
 
         // 1) SETUP X AXIS AS TIME
         // TIME
-        let timeScale = d3.scaleTime().range([0, width]);
-        // 2) GIVE X AXIS DATA
-        timeScale.domain(d3.extent(data, function(d) { return d.date; }));
+        let timeScale = d3.scaleTime()
+            .range([0, width])
+            .domain(d3.extent(data, function(d) { return d.date; }))
+        ;
 
         // CONTRIBUTORS
-        let contributorsScale = d3.scaleLinear().range([height, 0]);
+        let contributorsScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(data, function(d) {return Math.max(d.contributors);}) ])
+        ;
 
-        // ADDING VALUES CONTRIBUTORS
-        let controbutors = contributorsScale.domain([0, d3.max(data, function(d) {return Math.max(d.contributors);}) ]);
 
-        bars = svg.selectAll('.bar').data(data.map(item => { return item })).enter();
-
-        bars.append('rect')
-            .attr('class', 'bar-contributors')
+        bars = svg.selectAll('.bar')
+            .data(data.map(item => { return item })).enter()
+            .append('rect')
             .attr('x', function(d) { return timeScale(d.date) - (width / data.length) / 2; } )
-            .attr('width', (width / data.length) )
             .attr('y', function(d) { return contributorsScale(d.contributors); })
-            .attr('height', function(d) { return height - contributorsScale(d.contributors); } );
+            .attr('width', (width / data.length) )
+            .attr('height', function(d) { return height - contributorsScale(d.contributors); } )
+            .attr('class', 'bar-contributors')
+        ;
 
-        // STORY POINTS
-        let storyPointsScale = d3.scaleLinear().range([height, 0]);
-        // define the 2nd line
+        // STORY POINTS SCALE
+        let storyPointsScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(data, function(d) {return Math.max(d.storyPointsCompleted); })])
+        ;
+
+        // STORY POINTS LINE PATH
         let storyPointsLine = d3.line()
             .x(function(d) { return timeScale(d.date); })
-            .y(function(d) { return storyPointsScale(d.storyPointsCompleted); });
-
-        // ADDING VALUE STORY POINTS
-        storyPointsScale.domain([0, d3.max(data, function(d) {return Math.max(d.storyPointsCompleted); })]);
-
+            .y(function(d) { return storyPointsScale(d.storyPointsCompleted); })
+            .curve(d3.curveCatmullRom.alpha(0.1))
+        ;
 
         svg.append("path")
             .data([data])
@@ -101,9 +109,8 @@ let chart = d3.json('cadence-data.json').then(data => {
             .attr("transform", "translate( " + (width + 35 ) + ", 0 )")
             .call(d3.axisRight(storyPointsScale).ticks(10));
 
-
-
-
+    } else {
+        console.log('failed to load data!');
     }
 
-});
+}
